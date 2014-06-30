7b4d20787f78bd0062d2cad33b54fb18394a2d0a
pelo8888 <pelo8888@gmail.com>
2014-06-29 21:17:30 -0300

    Crear notificaciones, y algunas cosas mas

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,17 +1,17 @@
 
 $( document ).ready(function() {
     console.log( "ready!" );
-/* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-  $('body,html').animate({scrollTop:0},1000);
-})
-/* smooth scrolling for scroll down */
-$('.scroll-down').click(function(){
-  $('body,html').animate({scrollTop:$(window).scrollTop()+800},1000);
-})
+	/* smooth scrolling for scroll to top */
+	$('.scroll-top').click(function(){
+	  $('body,html').animate({scrollTop:0},1000);
+	})
+	/* smooth scrolling for scroll down */
+	$('.scroll-down').click(function(){
+	  $('body,html').animate({scrollTop:$(window).scrollTop()+800},1000);
+	})
 
-/* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#navbar' })
+	/* highlight the top nav as scrolling occurs */
+	$('body').scrollspy({ target: '#navbar' })
 });
 
 //UN DESTACADO
@@ -49,3 +49,177 @@ $('body').scrollspy({ target: '#navbar' })
 //     </div>
 //   </div>
 // </div>
+
+
+function crearNotificacion(id) {
+	var json = { 
+		  nombre : $('#' + id + ' .name').val(),
+      mail : $('#' + id + ' .email').val(),
+	    msg : $('#' + id + ' .msg').val(),
+	    tipo : id
+	}
+	confirmarSuscripcion(json);
+}
+
+function Buscar() {
+  buscarContenidos(filterSearch);
+}
+
+
+// Llamadas Ajax
+//GET___________GET_____________GET
+
+function buscarContenidos(filterSearch){
+
+	$.ajax({
+		url: CONF['ip'] + '/rwsinmueble/findbyfilters?filters=' + JSON.stringify(filterSearch),
+		type: 'GET',
+		dataType: 'json',
+	})
+	.done(function(msg) {
+    console.log(msg);
+  })
+  .fail(function(msg) {
+		console.log(msg);
+	})
+}
+
+function getPropiedad(id) {
+
+  bloquearPantalla();
+  $.ajax({
+      url: CONF['ip'] + '/rwsinmueble/' + id,
+      type: 'GET',
+      dataType: 'json',
+  })
+  .done(function(msg) {
+    console.log(msg);
+  })
+  .fail(function(msg) {
+    console.log(msg);
+  })
+  .always(function(msg) {
+    desbloquearPantalla();
+  });
+
+}
+
+function confirmarSuscripcion(obj) {
+  var query = 'emailCliente=' + obj.mail + 
+              '&tipoNotificacion=' + obj.tipo + 
+              '&mensaje=' + obj.msg;
+  bloquearPantalla();
+  $.ajax({
+			url: CONF['ip'] + '/rwsnotificacion/ingresarNotificacion?' + query,
+			type: 'GET',
+			dataType: 'json',
+	})
+	.done(function(msg) {
+		$('#' + obj.tipo).modal({show:false})
+	})
+	.fail(function(msg) {
+		console.log(msg);
+	})
+  .always(function(msg) {
+    desbloquearPantalla();
+  });
+}
+
+
+
+
+
+
+function bloquearPantalla(){
+    'use strict';
+    $.blockUI({ message: '<p><img src="images/home.gif" />  Cargando...</p>' ,
+        css: {
+            border: 'none',
+            padding: '15px',
+            backgroundColor: '#000',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            opacity: .5,
+            color: '#fff'
+        }
+    });
+}
+
+function desbloquearPantalla(){
+    'use strict';
+    $.unblockUI();
+}
+
+function confirmar(){
+	if(confirm('Estas seguro de continuar?'))
+		return true;
+	else
+		return false;
+}
+
+
+
+function cargarHtmlTop(topAux,idElem,pag){
+  pag || (pag = 0);
+
+  var lengthFor = confProy.CANT_CONTENIDOS_INDEX;
+  var html = "";
+  
+  if (topAux === undefined)
+    topAux = new Array();
+
+  if (topAux.length < confProy.CANT_CONTENIDOS_INDEX)
+    lengthFor = topAux.length;
+
+  for (var i = (pag*10 + 0); i < (pag*10 + 10); i++) {
+
+    if (!(i >= lengthFor)) {
+
+      var idContenido   = topAux[i].id;
+      var descripcion   = topAux[i].descripcionContenido;
+      var nombre        = topAux[i].nombreContenido;
+      var precio        = topAux[i].precio;
+      var calificacion  = topAux[i].calificacion;      
+      var foto          = '/Images?' + topAux[i].listaFotos[0];
+      var rutaDescarga  = topAux[i].urlDescarga;
+
+      var formatPrice = "USD " + precio;
+      if (precio == 0){
+        formatPrice = 'Gratis';
+        html += "<div class='col-sm-5 col-md-3'><div class='thumbnail'>"
+              + "<img src='" + foto + "' class='img-thumbnail miniatura img-responsive' style='width:200px;height:200px;'>"
+              + "<div class='caption'><h2 class='tituloTops'>" + nombre + "</h2>"
+              + "<p class='descripcionTops'>" + descripcion + "</p>"
+              + "<p class='precio'><a href='" + confProy.URL_CONTENIDO + idContenido + "' class='btn btn-primary' role='button'>Ver</a>"
+              + "<a href='" + rutaDescarga + "' onclick='return confirmar();' class='btn btn-default' role='button'>Descargar</a>"  
+              + formatPrice + "</p></div></div></div>";
+      }
+      else{
+        var valor = getCodePayPal(precio);
+        html += "<div class='col-sm-5 col-md-3'><div class='thumbnail'>"
+              + "<img src='" + foto + "' class='img-thumbnail miniatura img-responsive' style='width:200px;height:200px;'>"
+              + "<div class='caption'><h2 class='tituloTops'>" + nombre + "</h2>"
+              + "<p class='descripcionTops'>" + descripcion + "</p>"
+              + "<div class='row'>" 
+              + "<div class='col-md-5'>" 
+              + "<form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top'>"
+              + "<input type='hidden' name='cmd' value='_s-xclick'>"
+              + "<input type='hidden' name='hosted_button_id' value='" + valor + "'>"
+              + "<input type='image' src='https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!'>"
+              + "<img alt='' border='0' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1'>"
+              + "</form>"
+              + "</div>"              
+              + "<div class='col-md-7'>" 
+              + "<p class='precio'><a href='" + confProy.URL_CONTENIDO + idContenido + "' class='btn btn-primary' role='button'>Ver</a>"
+              + formatPrice + "</p>"
+              + "</div>"
+              + "</div>"
+              + "</div></div></div>";
+      }
+
+    };
+  };
+  var container = document.getElementById(idElem);
+      container.innerHTML = html;
+
+}
---
+++ assets/javascript/objets.js
@@ -0,0 +1,19 @@
+/* tipoBien es un array con valores dentro de  {local, casa, apartamento} */
+/* tipoTransaccion es un array con valores dentro de  {venta, alquiler} */
+/* el campo filtroStr se busca por todos los campos strings */
+/* el filtro cantidadBanios puede tenener los valores {1,2,3} (siendo 3, 3 o mas)*/
+/* el filtro cantidadHabilitaciones puede tenener los valores {1,2,3,4} (siendo 4, 4 o mas)*/
+/* el filtro barrios es un array con los ids de los barrios */
+
+CONF = { ip : '/plataforma-inmobiliaria/index.php' };
+
+filterSearch = {
+	tipoBien: ["apartamento","casa"], 
+	filtroStr : "",
+	tipoTransaccion : ["alquiler","venta"],
+	cantidadBanios : 4,
+	cantidadHabitaciones : 6,
+	barrios : [],
+	precioDesde : 1600,
+	precioHasta : 0,
+}
